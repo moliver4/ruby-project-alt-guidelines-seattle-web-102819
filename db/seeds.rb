4a1596f5293a7e6e88534a201c64f5249701322b
moliver4 <tifftang22@gmail.com>
2019-11-12 16:00:27 -0800

    added seeds for kids and tasks

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,18 @@
 
-# 5.times do 
 
-#     Kid.create(name: Faker::Name, age:  )
-# end
+Kid.create(name: Faker::Name, age: 19, skill_level: 3)
+Kid.create(name: Faker::Name, age: 15, skill_level: 3)
+Kid.create(name: Faker::Name, age: 7, skill_level: 2)
+Kid.create(name: Faker::Name, age: 6, skill_level: 2)
+
+Task.create(title: "Rake Leaves", difficulty: 2, reward: 2)
+Task.create(title: "Make Bed", difficulty: 1, reward: 1)
+Task.create(title: "Wash Dishes", difficulty: 3, reward: 3)
+Task.create(title: "Vaccuum", difficulty: 3, reward: 3)
+Task.create(title: "Sweep", difficulty: 3, reward: 2)
+Task.create(title: "Take Out Trash", difficulty: 2, reward: 1)
+Task.create(title: "Clean Up Room", difficulty: 2, reward: 5)
+Task.create(title: "Start Laundry", difficulty: 3, reward: 3)
+Task.create(title: "Fold Laundry", difficulty: 3, reward: 5)
+Task.create(title: "Pick Up Toys", difficulty: 1, reward: 1)
+Task.create(title: "Walk Dog", difficulty: 3, reward: 1)
