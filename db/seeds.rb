78f3abe7fd231d8f154062858cfb045dd0514938
moliver4 <tifftang22@gmail.com>
2019-11-12 16:05:12 -0800

    fixed kid and task seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,10 @@
+Kid.destroy_all
+Task.destroy_all
 
-
-Kid.create(name: Faker::Name, age: 19, skill_level: 3)
-Kid.create(name: Faker::Name, age: 15, skill_level: 3)
-Kid.create(name: Faker::Name, age: 7, skill_level: 2)
-Kid.create(name: Faker::Name, age: 6, skill_level: 2)
+Kid.create(name: "Becky", age: 19, skill_level: 3)
+Kid.create(name: "Brian", age: 15, skill_level: 3)
+Kid.create(name: "Jessica", age: 7, skill_level: 2)
+Kid.create(name: "Jason", age: 6, skill_level: 2)
 
 Task.create(title: "Rake Leaves", difficulty: 2, reward: 2)
 Task.create(title: "Make Bed", difficulty: 1, reward: 1)
@@ -16,3 +17,4 @@ Task.create(title: "Start Laundry", difficulty: 3, reward: 3)
 Task.create(title: "Fold Laundry", difficulty: 3, reward: 5)
 Task.create(title: "Pick Up Toys", difficulty: 1, reward: 1)
 Task.create(title: "Walk Dog", difficulty: 3, reward: 1)
+
